148d83dd244190bd4ba7d2be2888b17df2c562c4
Anthony Encarnacion <antho.2999@gmail.com>
2024-07-03 10:40:27 -0400

    Added new keybinds for harpoon2

--- init.lua
+++ init.lua
@@ -408,7 +408,7 @@ vim.o.termguicolors = true
 vim.cmd.colorscheme 'rose-pine'
 
 -- Character Column Limit
-vim.o.colorcolumn = "80"
+vim.o.colorcolumn = "100"
 --vim.api.nvim_set_hl(0, "colorcolumn", { bg = "#1b1b1b" })
 vim.cmd([[highlight ColorColumn ctermbg=238]])
 
@@ -696,8 +696,7 @@ require('which-key').register({
 require('mason').setup()
 require('mason-lspconfig').setup()
 require("mason-null-ls").setup({
-  ensure_installed = {
-  },
+  ensure_installed = {},
   handlers = {},
   automatic_installation = {},
 })
@@ -984,28 +983,48 @@ vim.keymap.set("n", "<C-e>",
   { desc = "Open harpoon window" }
 )
 vim.keymap.set("n", "<leader>aa",
-  function() harpoon:list():append() end,
-  { desc = "Append file to harpoon" }
+  function() harpoon:list():add() end,
+  { desc = "Add file to harpoon" }
 )
 vim.keymap.set("n", "<leader>ad", function() harpoon:list():remove() end,
   { desc = "Remove file from harpoon" }
 )
 
-vim.keymap.set("n", "<leader>ah",
+vim.keymap.set("n", "<leader>a1",
   function() harpoon:list():select(1) end,
   { desc = "Switch to file 1 in harpoon" }
 )
-vim.keymap.set("n", "<leader>aj",
+vim.keymap.set("n", "<leader>a2",
   function() harpoon:list():select(2) end,
   { desc = "Switch to file 2 in harpoon" }
 )
-vim.keymap.set("n", "<leader>ak",
+vim.keymap.set("n", "<leader>a3",
   function() harpoon:list():select(3) end,
   { desc = "Switch to file 3 in harpoon" }
 )
-vim.keymap.set("n", "<leader>al",
+vim.keymap.set("n", "<leader>a4",
   function() harpoon:list():select(4) end,
   { desc = "Switch to file 4 in harpoon" }
+)
+vim.keymap.set("n", "<leader>a5",
+  function() harpoon:list():select(5) end,
+  { desc = "Switch to file 5 in harpoon" }
+)
+vim.keymap.set("n", "<leader>a6",
+  function() harpoon:list():select(6) end,
+  { desc = "Switch to file 6 in harpoon" }
+)
+vim.keymap.set("n", "<leader>a7",
+  function() harpoon:list():select(7) end,
+  { desc = "Switch to file 7 in harpoon" }
+)
+vim.keymap.set("n", "<leader>a8",
+  function() harpoon:list():select(8) end,
+  { desc = "Switch to file 8 in harpoon" }
+)
+vim.keymap.set("n", "<leader>a9",
+  function() harpoon:list():select(9) end,
+  { desc = "Switch to file 9 in harpoon" }
 )
 
 
